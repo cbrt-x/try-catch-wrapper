22a265bf73209dccca72aecae4b1cf6ed1ad7816
Nils <Nils.Jadefalke@gmail.com>
2022-04-24 09:29:19 +0200

    ofSupplier/ofRunnable methods

--- src/main/java/io/github/jadefalke2/interfaces/UnsafeRunnable.java
+++ src/main/java/io/github/jadefalke2/interfaces/UnsafeRunnable.java
@@ -1,5 +1,10 @@
 package io.github.jadefalke2.interfaces;
 
 public interface UnsafeRunnable {
+
     void run () throws Exception;
+
+    static UnsafeRunnable ofRunnable (Runnable src) {
+        return src::run;
+    }
 }
--- src/main/java/io/github/jadefalke2/interfaces/UnsafeSupplier.java
+++ src/main/java/io/github/jadefalke2/interfaces/UnsafeSupplier.java
@@ -1,5 +1,12 @@
 package io.github.jadefalke2.interfaces;
 
+import java.util.function.Supplier;
+
 public interface UnsafeSupplier<T> {
+
     T supply() throws Exception;
+
+    static <T> UnsafeSupplier<T> ofSupplier (Supplier<T> src) {
+        return src::get;
+    }
 }
